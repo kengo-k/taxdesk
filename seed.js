e3e56d194bc7f6ee33aea2e51106fd76cd10b08c
kengo-k <31839234+kengo-k@users.noreply.github.com>
2024-04-21 01:33:24 +0000

    fix seed script

--- seed.js
+++ seed.js
@@ -6,6 +6,20 @@ const { execSync } = require("child_process");
 dotenv.config();
 
 function main() {
+  const executeStatement = createClient();
+
+  const files = fs.readdirSync("seed");
+  files.forEach((f) => {
+    const table = path.basename(f, ".csv");
+    let fullpath = path.resolve(`seed/${f}`);
+    executeStatement(`truncate table ${table}`);
+    executeStatement(
+      `\\copy ${table} FROM '${fullpath}' DELIMITER ',' CSV HEADER`
+    );
+  });
+}
+
+function createClient() {
   const url = process.env.DATABASE_URL;
 
   const pattern = /^postgresql:\/\/(.+):(.+)@(.+):(\d+)\/(.+?)(?:\?.*)?$/;
@@ -13,29 +27,24 @@ function main() {
 
   if (matches) {
     const [, user, password, host, port, db] = matches;
-
     console.log("user:", user);
     console.log("password:", password);
     console.log("host:", host);
     console.log("port:", port);
     console.log("database:", db);
     process.env.PGPASSWORD = password;
-
-    const files = fs.readdirSync("seed");
-    files.forEach((f) => {
-      const table = path.basename(f, ".csv");
-      let fullpath = path.resolve(`seed/${f}`);
+    return function (statement) {
       execSync(`
         psql \
           -U ${user} \
           -d ${db} \
           -h ${host} \
           -p ${port} \
-          -c "\\copy ${table} FROM '${fullpath}' DELIMITER ',' CSV HEADER"`);
-    });
-  } else {
-    console.log("Invalid URL format");
+          -c "${statement}"`);
+    };
   }
+
+  throw new Error();
 }
 
 main();
