40f24832f0fc5334bf4d145363a090e1eb72d11e
kengo-k <31839234+kengo-k@users.noreply.github.com>
2024-04-26 17:33:19 +0000

    add master service

---
+++ src/services/master.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from "@prisma/client";
+
+import { SaimokuSearchRequest, SaimokuSearchResponse } from "@/models/master";
+
+const prisma = new PrismaClient();
+
+export interface MasterService {
+  selectSaimokuDetail(
+    condition: SaimokuSearchRequest
+  ): Promise<SaimokuSearchResponse[]>;
+}
+
+export class MasterServiceImpl implements MasterService {
+  public async selectSaimokuDetail(
+    condition: SaimokuSearchRequest
+  ): Promise<SaimokuSearchResponse[]> {
+    const ledger_list = await prisma.$queryRaw<SaimokuSearchResponse[]>`
+    select
+      k.kamoku_cd,
+      s.saimoku_cd,
+      b.kamoku_bunrui_type
+    from
+      saimoku_masters s
+        inner join kamoku_masters k on
+          k.kamoku_cd = s.kamoku_cd
+        inner join kamoku_bunrui_masters b on
+          b.kamoku_bunrui_cd = k.kamoku_bunrui_cd
+    where
+      saimoku_cd = ${condition.saimoku_cd}`;
+    return ledger_list;
+  }
+}
