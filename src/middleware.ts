e7661c36354d32245ae87d0eaa001f725dfbdb35
kengo-k <31839234+kengo-k@users.noreply.github.com>
2024-06-05 11:14:55 +0900

    middleware: refactor

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,56 @@
-// middleware.ts
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
-export function middleware(request: NextRequest) {
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export async function middleware(request: NextRequest) {
+  const jwt = request.cookies.get('tax_account_app_auth_token')?.value
+  const authorizationHeader = request.headers.get('authorization')
+
   const path = request.nextUrl.pathname
   console.log('Middleware called from ', path)
+
   if (path.startsWith('/login')) {
     return NextResponse.next()
   }
+
   if (path.startsWith('/api/public')) {
     return NextResponse.next()
   }
 
-  const token = request.cookies.get('token')?.value
-  if (!token) {
-    if (path.startsWith('/api')) {
-    } else {
-      return NextResponse.redirect(new URL('/login', request.url))
+  if (path.startsWith('/api')) {
+    if (!authorizationHeader) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+    }
+
+    const token = authorizationHeader.replace('Bearer ', '')
+
+    try {
+      const { error } = await supabase.auth.getUser(token)
+      if (error) {
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+      }
+      return NextResponse.next()
+    } catch (error) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
     }
   }
-  // if (path.startsWith('/api')) {
-  //   const token = request.headers.get('Authorization')?.split('Bearer ')[1]
-  //   if (!token) {
-  //     return NextResponse.json(
-  //       { error: 'Authentication token not provided.' },
-  //       { status: 401 },
-  //     )
-  //   }
-  // }
+
+  if (!jwt) {
+    if (path !== '/login') {
+      const loginUrl = new URL('/login', request.url)
+      return NextResponse.redirect(loginUrl)
+    }
+  } else {
+    if (path === '/login') {
+      const homeUrl = new URL('/', request.url)
+      return NextResponse.redirect(homeUrl)
+    }
+  }
 
   return NextResponse.next()
 }
