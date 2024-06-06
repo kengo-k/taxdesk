c002c93e5b246dc4400cadf2f22caf356134ee34
kengo-k <31839234+kengo-k@users.noreply.github.com>
2024-06-06 10:16:59 +0900

    Fix to the target of login checking  to API only

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,27 +8,18 @@ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export async function middleware(request: NextRequest) {
-  const jwt = request.cookies.get('tax_account_app_auth_token')?.value
-  const authorizationHeader = request.headers.get('authorization')
-
   const path = request.nextUrl.pathname
-  console.log('Middleware called from ', path)
-
-  if (path.startsWith('/login')) {
-    return NextResponse.next()
-  }
-
   if (path.startsWith('/api/public')) {
     return NextResponse.next()
   }
 
   if (path.startsWith('/api')) {
+    const authorizationHeader = request.headers.get('authorization')
     if (!authorizationHeader) {
       return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
     }
 
     const token = authorizationHeader.replace('Bearer ', '')
-
     try {
       const { error } = await supabase.auth.getUser(token)
       if (error) {
@@ -37,18 +28,6 @@ export async function middleware(request: NextRequest) {
       return NextResponse.next()
     } catch (error) {
       return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
-    }
-  }
-
-  if (!jwt) {
-    if (path !== '/login') {
-      const loginUrl = new URL('/login', request.url)
-      return NextResponse.redirect(loginUrl)
-    }
-  } else {
-    if (path === '/login') {
-      const homeUrl = new URL('/', request.url)
-      return NextResponse.redirect(homeUrl)
     }
   }
 
