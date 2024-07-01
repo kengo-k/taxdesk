722b84992af07f31d47681999dd4c950b4f43a69
kengo-k <31839234+kengo-k@users.noreply.github.com>
2024-07-01 22:50:01 +0900

    #2 Fixed problem with validation errors when creating new records

--- src/app/ledger/page.tsx
+++ src/app/ledger/page.tsx
@@ -275,16 +275,6 @@ const LedgerList: FC<{
     })
   }, [ledger_state.ledger_list, saimoku_map]) // eslint-disable-line react-hooks/exhaustive-deps
 
-  useEffect(() => {
-    if (ledger_state.last_upserted !== null) {
-      dispatch(ledgerActions.clearLastUpserted())
-      const current_values = create_form.values
-      create_form.reset()
-      create_form.setFieldValue('nendo', current_values.nendo)
-      create_form.setFieldValue('date_yymm', current_values.date_yymm)
-    }
-  }, [dispatch, ledger_state.last_upserted]) // eslint-disable-line react-hooks/exhaustive-deps
-
   return (
     <div>
       <Title>
@@ -487,7 +477,6 @@ const LedgerListNewRow: FC<{
         ],
       }),
     )
-    date_ref.current?.focus()
   }
 
   const onSave = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -515,6 +504,21 @@ const LedgerListNewRow: FC<{
   const date_ref = useRef<HTMLInputElement>(null)
   const counter_cd_ref = useRef<HTMLInputElement>(null)
   const note_ref = useRef<HTMLInputElement>(null)
+
+  const { data: ledger_state } = useSelector((state: RootState) => state.ledger)
+
+  useEffect(() => {
+    if (ledger_state.last_upserted !== null) {
+      dispatch(ledgerActions.clearLastUpserted())
+      const current_values = props.form.values
+      props.form.reset()
+      props.form.setFieldValue('nendo', current_values.nendo)
+      props.form.setFieldValue('date_yymm', current_values.date_yymm)
+      setTimeout(() => {
+        date_ref.current?.focus()
+      }, 0)
+    }
+  }, [dispatch, ledger_state.last_upserted, date_ref]) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <tr>
