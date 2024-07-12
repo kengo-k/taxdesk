d04abe16e9e1ef93d36bdac1219d258478f03eab
kengo-k <31839234+kengo-k@users.noreply.github.com>
2024-07-12 22:59:58 +0900

    #10 Fix nendo api to use common func

--- src/app/api/v1/masters/nendo/route.ts
+++ src/app/api/v1/masters/nendo/route.ts
@@ -1,28 +1,10 @@
-import { NextResponse } from 'next/server'
-
-import { getDefault } from '@/constants/cache'
-import { UNEXPECTED_ERROR } from '@/constants/error'
 import { Factory } from '@/dicontainer'
-import { ApiResponse } from '@/misc/types'
+import { ApiResponse, execApi } from '@/misc/types'
 
 export const dynamic = 'force-dynamic'
-const cache = getDefault()
 
-export async function GET() {
-  try {
-    const service = Factory.getMasterService()
-    const nendo_list = await service.selectNendoList()
-    return NextResponse.json(ApiResponse.success(nendo_list), {
-      status: 200,
-      headers: cache.headers,
-    })
-  } catch {
-    return NextResponse.json(
-      ApiResponse.failureWithAppError(UNEXPECTED_ERROR),
-      {
-        status: 500,
-        headers: cache.headers,
-      },
-    )
-  }
-}
+export const GET = execApi(async () => {
+  const service = Factory.getMasterService()
+  const nendo_list = await service.selectNendoList()
+  return ApiResponse.success(nendo_list)
+})
