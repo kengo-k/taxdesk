397bb2ebbff743a59cd0671cf96a9e95760aa4f6
kengo-k <31839234+kengo-k@users.noreply.github.com>
2024-07-13 18:53:44 +0900

    #10 chore: fix login api

--- src/app/api/public/login/route.ts
+++ src/app/api/public/login/route.ts
@@ -60,7 +60,7 @@ export async function POST(request: NextRequest) {
         }),
       )
     }
-  } catch (error) {
+  } catch {
     return NextResponse.json(
       { error: 'Internal server error' },
       { status: 500 },
--- src/app/api/public/verify/route.ts
+++ src/app/api/public/verify/route.ts
@@ -14,68 +14,75 @@ export async function POST(request: NextRequest) {
     })
   }
 
-  const { data: user_data, error: user_error } =
-    await supabase.auth.signInWithPassword({
-      email,
-      password,
-    })
+  try {
+    const { data: user_data, error: user_error } =
+      await supabase.auth.signInWithPassword({
+        email,
+        password,
+      })
 
-  if (user_error) {
-    return NextResponse.json(ApiResponse.failure(AUTH_ERROR()), {
-      status: 401,
-    })
-  }
-
-  // get auth level
-  const { data: level, error: level_error } =
-    await supabase.auth.mfa.getAuthenticatorAssuranceLevel()
-
-  if (level_error) {
-    return NextResponse.json(ApiResponse.failure(AUTH_ERROR()), {
-      status: 401,
-    })
-  }
-
-  // return response because authentication has already been completed.
-  if (level.currentLevel === level.nextLevel) {
-    return NextResponse.json(ApiResponse.success('Login succeeded.'))
-  }
-
-  if (level.currentLevel === 'aal1' && level.nextLevel === 'aal2') {
-    const factors = user_data.user.factors
-    if (!factors) {
+    if (user_error) {
       return NextResponse.json(ApiResponse.failure(AUTH_ERROR()), {
         status: 401,
       })
     }
-    let auth = false
-    if (factors) {
-      for (const factor of factors) {
-        const { data: verify_data } =
-          await supabase.auth.mfa.challengeAndVerify({
-            factorId: factor.id,
-            code,
-          })
-        if (verify_data) {
-          auth = true
-          break
-        }
-      }
-    }
-    if (auth) {
-      const cookie = cookies()
-      cookie.set('sign', user_data.session.access_token, {
-        httpOnly: true,
-        secure: false,
-        sameSite: 'strict',
-        maxAge: 60 * 60 * 24,
-        path: '/',
+
+    // get auth level
+    const { data: level, error: level_error } =
+      await supabase.auth.mfa.getAuthenticatorAssuranceLevel()
+
+    if (level_error) {
+      return NextResponse.json(ApiResponse.failure(AUTH_ERROR()), {
+        status: 401,
       })
+    }
+
+    // return response because authentication has already been completed.
+    if (level.currentLevel === level.nextLevel) {
       return NextResponse.json(ApiResponse.success('Login succeeded.'))
     }
-  }
 
-  return NextResponse.json(ApiResponse.failure(AUTH_ERROR()), {
-    status: 401,
-  })
+    if (level.currentLevel === 'aal1' && level.nextLevel === 'aal2') {
+      const factors = user_data.user.factors
+      if (!factors) {
+        return NextResponse.json(ApiResponse.failure(AUTH_ERROR()), {
+          status: 401,
+        })
+      }
+      let auth = false
+      if (factors) {
+        for (const factor of factors) {
+          const { data: verify_data } =
+            await supabase.auth.mfa.challengeAndVerify({
+              factorId: factor.id,
+              code,
+            })
+          if (verify_data) {
+            auth = true
+            break
+          }
+        }
+      }
+      if (auth) {
+        const cookie = cookies()
+        cookie.set('sign', user_data.session.access_token, {
+          httpOnly: true,
+          secure: false,
+          sameSite: 'strict',
+          maxAge: 60 * 60 * 24,
+          path: '/',
+        })
+        return NextResponse.json(ApiResponse.success('Login succeeded.'))
+      }
+    }
+
+    return NextResponse.json(ApiResponse.failure(AUTH_ERROR()), {
+      status: 401,
+    })
+  } catch {
+    return NextResponse.json(
+      { error: 'Internal server error' },
+      { status: 500 },
+    )
+  }
 }
