2c3167f460245a6bd3a069eacfe8cb929f31a626
kengo-k <31839234+kengo-k@users.noreply.github.com>
2025-05-26 09:47:28 +0900

    #51 Fix parameters2024 for saimoku settings change

--- lib/client/tax-calculation/parameters/parameters2024.ts
+++ lib/client/tax-calculation/parameters/parameters2024.ts
@@ -152,14 +152,7 @@ export const parameters2024Builder: ParameterBuilder = (
     // 事業の費用および営業外費用を全て加算する
     // 租税公課のうち事業税は翌年度に損金参入するため当年度からは除外する
     // (昨年度から引き継いだ)未払法人税のうち事業税は当年度に損金参入するため加算する
-    params.expenses =
-      state[2].response[0].value -
-      state[3].response.reduce((acc, item) => {
-        if (item.custom_fields?.category === 'exclude_expense') {
-          return acc + item.value
-        }
-        return acc
-      }, 0)
+    params.expenses = state[2].response[0].value
 
     params.previous_business_tax = state[4].response.reduce((acc, item) => {
       if (item.custom_fields?.category === 'include_expense') {
