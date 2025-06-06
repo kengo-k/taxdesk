280027c2f97e9106ee2b317d0faef167b916a017
kengo-k <31839234+kengo-k@users.noreply.github.com>
2025-06-06 09:35:03 +0900

    #51 Remove unused apis

--- app/api/fiscal-years/[year]/reports/balance-sheet/route.ts
+++
@@ -1,8 +0,0 @@
-import { createApiRoute, withTransaction } from '@/lib/backend/api-transaction'
-import { getFiscalYears } from '@/lib/backend/services/masters/get-fiscal-years'
-
-export const GET = createApiRoute(async (db) => {
-  return withTransaction(db, async (tx) => {
-    return await getFiscalYears(tx)
-  })
-})
